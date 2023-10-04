a99228e9e7170bc3d3384268e2d5d824381ccc01
raid-salhi <118809948+raid-salhi@users.noreply.github.com>
2023-10-04 18:06:07 +0100

    draw the body

---
+++ app/src/main/java/com/example/bottleofwater/BottleOfWater.kt
@@ -0,0 +1,75 @@
+package com.example.bottleofwater
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.clipPath
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BottleOfWater(
+    modifier: Modifier,
+    maxVolume:Int=2400,
+    currentVolume:Int=0,
+    waterColor:Color=Color(0xFF03A9F4),
+    capColor:Color= Color(0xFF3F51B5),
+    bottleColor:Color= Color.White
+) {
+    Box(modifier = modifier
+        .width(200.dp)
+        .height(600.dp)){
+        Canvas(modifier = Modifier.fillMaxSize()){
+            val width = size.width
+            val height= size.height
+            val bodyBottlePath = Path().apply {
+                moveTo(x = width*0.3f,y=height *0.1f)
+                lineTo(
+                    x = width*0.3f , y=height * 0.2f
+                )
+                quadraticBezierTo(
+                    x1  = width*0.15f, y1 = height * 0.3f,
+                    x2 = 0f , y2 = height *0.4f
+                )
+                lineTo(
+                    x = 0f , y=height * 0.95f
+                )
+                quadraticBezierTo(
+                    x1  = 0f, y1 = height ,
+                    x2 = width*0.05f , y2 = height
+                )
+                lineTo(
+                    x = width *0.95f , y=height
+                )
+                quadraticBezierTo(
+                    x1  = width, y1 = height ,
+                    x2 = width , y2 = height*0.95f
+                )
+                lineTo(
+                    x = width , y=height *0.4f
+                )
+                quadraticBezierTo(
+                    x1  = width*0.85f, y1 = height * 0.3f,
+                    x2 = 0.7f , y2 = height *0.2f
+                )
+                lineTo(
+                    x = width*0.7f , y=height * 0.1f
+                )
+                close()
+            }
+            clipPath(
+                bodyBottlePath
+            ){
+                drawRect(
+                    color=bottleColor,
+                    size=size
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/bottleofwater/MainActivity.kt
+++ app/src/main/java/com/example/bottleofwater/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.bottleofwater
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.bottleofwater.ui.theme.BottleOfWaterTheme
 
@@ -22,25 +27,16 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.background(color = Color.Gray)
+                    ) {
+                        BottleOfWater(modifier = Modifier)
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BottleOfWaterTheme {
-        Greeting("Android")
-    }
-}
